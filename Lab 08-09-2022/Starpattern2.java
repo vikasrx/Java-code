6412cc250476c4baa8344db038855dfd52ea66ef
Vikas Meena <63559391+vikasrx@users.noreply.github.com>
2022-09-08 21:33:18 +0530

    Add files via upload

---
+++ Lab 08-09-2022/Starpattern2.java
@@ -0,0 +1,21 @@
+class Starpattern2{
+       
+          public static void main(String args[]){
+		
+			for (int i=0; i<5; i++){
+				
+                            for(int j=0; j<5; j++){
+				
+                                    if(i==j || i+j==5-1){
+				
+                                    System.out.print("*");
+			                 }
+			                  else{
+				            System.out.print(" ");
+			           }	
+                 
+	             	}
+			                  System.out.println();
+		}
+	}
+}
