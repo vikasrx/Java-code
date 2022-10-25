1844e14fb7bf0f384f08860f20d8dba2933cacbf
Vikas Meena <63559391+vikasrx@users.noreply.github.com>
2022-10-25 15:04:02 +0530

    Add files via upload

---
+++ Area of Shape/AbstractArea.java
@@ -0,0 +1,11 @@
+package abstractpack;
+
+public abstract class AbstractArea {
+	
+	abstract double areaOfReactangle(int length, int breadth);
+	
+	abstract double areaOfCircle(int radius);
+	
+	abstract double areaOfSquare(int side);
+	
+}
